ff0e1e0eab9ff5424c3968c80d6b091a98d9b45d
Nikolay Digaev <ffs.cmp@gmail.com>
2016-11-29 15:13:23 +0500

    [API] Add `pddQuestion` association to `ExamQuestion` model

--- app/ExamQuestion.php
+++ app/ExamQuestion.php
@@ -7,16 +7,22 @@ use Illuminate\Database\Eloquent\Model;
 class ExamQuestion extends Model
 {
     protected $fillable = [
-        'exam_id', 'number', 'answer'
+        'exam_id', 'pdd_question_id', 'number', 'answer'
     ];
 
     protected $casts = [
         'exam_id' => 'integer',
+        'pdd_question_id' => 'integer',
         'number' => 'integer'
     ];
 
     public function exam()
     {
         return $this->belongsTo('App\Exam');
+    }
+
+    public function pddQuestion()
+    {
+        return $this->belongsTo('App\PddQuestion');
     }
 }
